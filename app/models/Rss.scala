d97bff9b12f5ec225c896ecaacac2db0683cb28d
Asari Mitsuteru <asari.mtr@gmail.com>
2012-08-06 00:04:45 +0900

    RSS取得を追加

--- app/models/Rss.scala
+++ app/models/Rss.scala
@@ -1,11 +1,31 @@
 package models
 
+import scala.xml.XML
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.Locale
 import java.util.Date
 
 case class Rss(val title:String, val link:String, val pubDate:Date)
 
 object Rss {
   def get():List[Rss] = {
-      Rss("hello", "yahoo.co.jp", new Date()) :: Nil
+
+    val xml = XML.load(new URL("http://rss.dailynews.yahoo.co.jp/fc/rss.xml"))
+
+    val format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.US)
+
+    var items = List.empty[Rss]
+
+    for (item <- xml \\ "item") {
+        val pubDate = (item \ "pubDate").text
+        val date = format.parse(pubDate)
+        items = Rss(
+            ((item \ "title").text),
+            ((item \ "link").text),
+            date) :: items
+    }
+
+    items
   }
 }
