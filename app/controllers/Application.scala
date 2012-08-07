1ef4ca752e17bedbceb7c97ef68b5cfffdbb3376
Asari Mitsuteru <asari.mtr@gmail.com>
2012-08-08 07:30:55 +0900

    WSで実装

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,8 @@ import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.json.Json._
 
+import play.api.libs.ws.WS
+
 import models.Rss
 
 object Application extends Controller {
@@ -15,9 +17,14 @@ object Application extends Controller {
   }
 
   def read = Action {
-    Ok(toJson(
-        Map("status" -> "OK", "message" -> "Hello World")
-    ))
+    Async {
+      WS.url("http://rss.dailynews.yahoo.co.jp/fc/rss.xml").get().map { response =>
+        val item = Rss.getYahoo(response.body)
+        Ok(toJson(
+            Map("status" -> "OK", "message" -> response.body.toString)
+        ))
+      }
+    }
   }
   
 }
--- app/models/Rss.scala
+++ app/models/Rss.scala
@@ -9,6 +9,27 @@ import java.util.Date
 case class Rss(val title:String, val link:String, val pubDate:Date)
 
 object Rss {
+
+  def getYahoo(xmlData:String):List[Rss] = {
+
+    val xml = XML.loadString(xmlData)
+
+    val format = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.US)
+
+    var items = List.empty[Rss]
+
+    for (item <- xml \\ "item") {
+        val pubDate = (item \ "pubDate").text
+        val date = format.parse(pubDate)
+        items = Rss(
+            ((item \ "title").text),
+            ((item \ "link").text),
+            date) :: items
+    }
+
+    items
+  }
+
   def get():List[Rss] = {
 
     val xml = XML.load(new URL("http://rss.dailynews.yahoo.co.jp/fc/rss.xml"))
