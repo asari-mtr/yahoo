cc69fa9b00b1a578ba202113cea64f8feb18134f
Asari Mitsuteru <asari.mtr@gmail.com>
2012-08-08 00:02:05 +0900

    json 対応

--- app/assets/javascripts/main.coffee
+++ app/assets/javascripts/main.coffee
@@ -1,2 +1,7 @@
 $ ->
-    alert("Hello")
+    url = "http://localhost:9000/api/read"
+    $.getJSON(url, (data, status) ->
+        alert(data.message)
+        return
+    )
+    return
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,12 +3,21 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import play.api.libs.json._
+import play.api.libs.json.Json._
+
 import models.Rss
 
 object Application extends Controller {
   
   def index = Action {
     Ok(views.html.index(Rss.get()))
+  }
+
+  def read = Action {
+    Ok(toJson(
+        Map("status" -> "OK", "message" -> "Hello World")
+    ))
   }
   
 }
